05eb3b9f4f4d26c509f9395d5b0c17d46d56ee94
SultanXPro <evening.tea@mail.ru>
2023-02-05 17:50:47 +0600

    sula

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello my popkaa");
+        Scanner scan = new Scanner(System.in);
+        int [] a;
+        int b;
+        System.out.println("please enter array num");
+        b = scan.nextInt();
+        a = new int[b];
+        for (int i = 0; i < b; i++){
+            System.out.println("Only Discipline");
+            b = scan.nextInt();
+        }
     }
 }
